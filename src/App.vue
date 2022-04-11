f80323e9688242e10b5737dcd685d08bfa04764c
andrea1632 <andrea.bellucci10@libero.it>
2022-04-11 15:15:46 +0200

    es completo + bonus

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TitleMain></TitleMain>
+    <HeroSection></HeroSection>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import TitleMain from './components/TitleMain.vue'
+import HeroSection from './components/jumbotron.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+    TitleMain, 
+    HeroSection
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,13 @@
+<template>
+    <img src="https://smartslider3.com/wp-content/uploads/2018/07/createwordpressheroimage-780x410.png" alt="jumbotron image">
+</template>
+<script>
+export default {
+  name: 'HeroSection',
+}
+</script>
+<style scoped>
+  img{
+    width: 100%;
+  }
+</style>
